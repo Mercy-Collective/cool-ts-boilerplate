962b389754b6e05caca5064910709a97601c63c6
cool <firefox404@hotmail.com>
2022-11-20 17:36:21 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+
+description 'Cool - Boilerplate'
+author 'cool'
+version '0.0.1'
+
+lua54 'yes'
+
+game 'gta5'
+
+client_scripts {
+    'client/*.js',
+}
+
+server_scripts {
+    'server/*.js',
+}
